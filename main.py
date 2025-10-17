1cdd7bceefa78dfec942e450c272c1772aaa73d2
acarkayrakerem <acarkayrakerem@protonmail.com>
2025-10-17 13:12:20 +0200

    Add Izmir

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ driverIst.get("https://iski.istanbul/baraj-doluluk/")
 driverBursa = webdriver.Chrome()
 driverBursa.get("https://www.buski.gov.tr/baraj-detay")
 
+driverAnkara = webdriver.Chrome()
+driverAnkara.get("https://www.aski.gov.tr/tr/baraj.aspx")
+
+driverIzmir = webdriver.Chrome()
+driverIzmir.get("https://www.izsu.gov.tr/tr/BarajlarinSuDurumu/1")
+
 time.sleep(5)
 
 # İstanbul 
@@ -20,8 +26,38 @@ soup2 = BeautifulSoup(driverBursa.page_source, "html.parser")
 ratio2 = soup2.find("span", {"id": "baraj-doluluk-1-info"}).get_text(strip=True)
 ratio2_num = float(ratio2.replace('%', '').replace(',', '.'))
 
-print("İstanbul Baraj Doluluk Oranı:", ratio1)
-print("Bursa Baraj Doluluk Oranı:", ratio2)
+# İzmir 
+soup3 = BeautifulSoup(driverIzmir.page_source, "html.parser")
+
+toplam_row = soup3.find("span", string=lambda x: x and "Kullanılabilir göl su hacmi" in x).find_parent("tr")
+toplam_values = [
+    float(td.get_text(strip=True).replace('.', '').replace(',', '.'))
+    for td in toplam_row.find_all("td", class_="damtotaltd") if td.get_text(strip=True)
+]
+toplam_sum = sum(toplam_values)
+
+kullanilabilir_row = soup3.find("span", string=lambda x: x and "Kullanılabilir su hacmi" in x).find_parent("tr")
+kullanilabilir_values = [
+    float(td.get_text(strip=True).replace('.', '').replace(',', '.'))
+    for td in kullanilabilir_row.find_all("td", class_="damtotaltd") if td.get_text(strip=True)
+]
+kullanilabilir_sum = sum(kullanilabilir_values)
+
+ratio3 = round( kullanilabilir_sum / toplam_sum * 100,2)
+ratio3_num = ratio3
+
+#Ankara
+soup4 = BeautifulSoup(driverAnkara.page_source, "html.parser")
+ratio4 = soup4.find("label", {"id": "LabelBarajOrani"}).get_text(strip=True)
+ratio4_num = float(ratio4.replace('%', '').replace(',', '.'))
+
+print("İstanbul Baraj Doluluk Oranı: %", ratio1_num)
+print("Bursa Baraj Doluluk Oranı: %", ratio2_num)
+print("İzmir Baraj Doluluk Oranı: %", ratio3_num)
+print("Ankara Baraj Doluluk Oranı: %", ratio4_num)
+
 
 driverIst.quit()
 driverBursa.quit()
+driverAnkara.quit()
+driverIzmir.quit()
